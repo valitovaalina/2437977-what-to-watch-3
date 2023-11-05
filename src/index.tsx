4a600d550fb4491c0cb495a5a7868e6482797c5e
Valitova Alina <valitovaalina1501@gmail.com>
2023-11-05 13:58:03 +0500

    Поправила ошибки

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@components/app/app';
-import { films } from '@mocks/films';
 import { store } from './store';
 
 const Setting = {
@@ -18,7 +17,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App filmCardTitle={Setting.filmCardTitle} filmCardGenre={Setting.filmCardGenre} filmCardYear={Setting.filmCardYear} films={films} />
+      <App filmCardTitle={Setting.filmCardTitle} filmCardGenre={Setting.filmCardGenre} filmCardYear={Setting.filmCardYear} />
     </Provider>
   </React.StrictMode>
 );
